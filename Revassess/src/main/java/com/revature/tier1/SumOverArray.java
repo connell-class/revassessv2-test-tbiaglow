062014063d1e926ea660ebb23cbd0ce4a27beb9f
Thomas Biaglow <tbiaglow@gmail.com>
2020-07-09 15:11:33 -0400

    testing SumOverArray

--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -4,8 +4,8 @@ public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
 		int length = 0;
-		for (int num : arr) {
-			length += num;
+		for (int i = 0; i < arr.length; i++) {
+			length += arr[i];
 		}
 		return length;
 	}
