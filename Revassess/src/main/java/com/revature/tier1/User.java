2bf207aa46bce2c6afe3a19d8e87325f3f3160eb
Thomas Biaglow <tbiaglow@gmail.com>
2020-07-09 13:41:28 -0400

    testing User

--- Revassess/src/main/java/com/revature/tier1/User.java
+++ Revassess/src/main/java/com/revature/tier1/User.java
@@ -1,4 +1,10 @@
 package com.revature.tier1;
 
 public class User {
+	String name = "Bob";
+	Integer id = 1124;
+	protected boolean isAuthorized = false;
+	void decrementId () {
+		this.id -= 1;
+	}
 }
