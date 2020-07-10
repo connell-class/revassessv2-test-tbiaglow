f0fd66d84a787d5ee62bc965bfed11934352cd21
Thomas Biaglow <tbiaglow@gmail.com>
2020-07-10 00:47:51 -0400

    testing answer2.sql in tier2

--- Revassess/src/sql/answer2.sql
+++ Revassess/src/sql/answer2.sql
@@ -1,2 +1,2 @@
-insert into APP_USER (user_id, username, password, first_name, last_name, role_id)
-values (27, 'testuser', 'password', 'Bob', 'Smith', 4);
+insert into app_user (user_id, username, password, first_name, last_name, role_id)
+values (100, 'testuser', 'password', 'Bob', 'Smith', 4);
