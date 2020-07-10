dc4e8da14d814132ec0644544540c6590e836942
Thomas Biaglow <tbiaglow@gmail.com>
2020-07-10 11:05:34 -0400

    testing pointstests

--- Revassess/src/main/java/com/revature/tier1/CompareStrings.java
+++ Revassess/src/main/java/com/revature/tier1/CompareStrings.java
@@ -3,8 +3,8 @@ package com.revature.tier1;
 public class CompareStrings {
 
     public static boolean compareStrings(String s1, String s2){
-    	if (s1.equals(s2)) {return true;} else {
+//    	if (s1.equals(s2)) {return true;} else {
         return false;
-    	}
+//    	}
     }
 }
--- Revassess/src/main/java/com/revature/tier1/User.java
+++ Revassess/src/main/java/com/revature/tier1/User.java
@@ -1,10 +1,10 @@
 package com.revature.tier1;
 
 public class User {
-	Integer id;
-	String firstName;
-	String lastName;
-	String userName;
-	String password;
-	String role;
+//	Integer id;
+//	String firstName;
+//	String lastName;
+//	String userName;
+//	String password;
+//	String role;
 }
