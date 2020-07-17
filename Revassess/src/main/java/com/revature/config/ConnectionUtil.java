6de863628d9ebf857ac8d217a1039decf419b333
Thomas Biaglow <tbiaglow@gmail.com>
2020-07-17 01:05:16 -0400

    testing tier 3

--- Revassess/src/main/java/com/revature/config/ConnectionUtil.java
+++ Revassess/src/main/java/com/revature/config/ConnectionUtil.java
@@ -1,6 +1,9 @@
 package com.revature.config;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * 
@@ -16,25 +19,55 @@ public class ConnectionUtil {
 	private static ConnectionUtil cu;
 	
 	// add your jdbc url
-	public static final String URL = "";
+	public static final String URL = "jdbc:oracle:thin:@rev2004.crnv2jkczn6e.us-east-2.rds.amazonaws.com:1521:ORCL";
 	// add your jdbc username
-	public static final String USERNAME = "";
+	public static final String USERNAME = "admin";
 	// add your jdbc password
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "password";
 	// name of the created stored procedure in tier 3
-	public static final String TIER_3_PROCEDURE_NAME = "";
+	public static final String TIER_3_PROCEDURE_NAME = "user_study_sets";
 	// name of the created sequence in tier 3
 	public static final String TIER_3_SEQUENCE_NAME = "";
 
 	// implement this method to connect to the db and return the connection object
+//	public Connection connect(){
+//		return null;
+//	}
 	public Connection connect(){
-		return null;
+		Connection conn = null;
+		try
+		{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} // end try
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		} // end catch
+		return conn;
 	}
 
 
 	//implement this method with a callable statement that calls the absolute value sql function
+//	public long callAbsoluteValueFunction(long value){
+//		return value;
+//	}
 	public long callAbsoluteValueFunction(long value){
-		return value;
+		long abs = 0;
+		try (Connection conn = cu.connect()) {
+
+			String sql = "select ABS(?) from dual";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, value);
+
+			ResultSet rs = pstmt.executeQuery(sql);
+			
+			abs = rs.getLong(0);
+
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		}
+		return abs;
 	}
 	
 
--- Revassess/src/main/java/com/revature/model/Role.java
+++ Revassess/src/main/java/com/revature/model/Role.java
@@ -1,5 +1,5 @@
 package com.revature.model;
 
 public enum Role {
-
+	ADMIN, DEV, BASIC_USER, PREMIUM_USER, LOCKED;
 }
